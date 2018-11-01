c335679c052d4f9168923cd55c5250c5220da98a
liubo.home <liubo@home.com>
2018-11-01 23:54:51 +0800

    static web

---
+++ main.go
@@ -0,0 +1,25 @@
+/**
+ * Auth :   liubo
+ * Date :   2018/11/1 23:41
+ * Comment: 简单的静态网站服务器
+ */
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := flag.String("p", "8100", "port to serve on")
+	directory := flag.String("d", ".", "the directory of static file to host")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
+
+	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
+
